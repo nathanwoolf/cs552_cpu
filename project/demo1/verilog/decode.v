b78efd8149e88a15aa96b3c0168ea325f5c599cf
nathanwoolf <nathanwoolf38@gmail.com>
2024-10-18 17:00:07 -0500

    finished fetch? began decode

--- project/demo1/verilog/decode.v
+++ project/demo1/verilog/decode.v
@@ -5,11 +5,42 @@
    Description     : This is the module for the overall decode stage of the processor.
 */
 `default_nettype none
-module decode (/* TODO: Add appropriate inputs/outputs for your decode stage here*/);
+module decode (input clk, 
+               input rst, 
+               input [15:0]instr, 
+               output aluSrc, 
+               output zeroExt, 
+               output memToReg, 
+               output regWrite, 
+               output memRead, 
+               output memWrite, 
+               output jump, 
+               output immSource, 
+               output [2:0]aluOp, 
+               output invA, 
+               output invB, 
+               output cin, 
+               output brControl); 
 
    // TODO: Your code here
-
-   // instatiate control module
    
+   // control module signals
+   wire [1:0]aluSrc;
+   wire zeroExt; 
+   wire memToReg;
+   wire regWrite; 
+   wire memRead; 
+   wire memWrite;
+   wire jump; 
+   wire immSource; 
+   wire [2:0]aluOp; 
+   wire invA; 
+   wire invB; 
+   wire cin; 
+   wire [1:0]brControl; 
+   
+   // instatiate control module
+   control CONTROLSIGS(.opcode(instr[15:11]), )
+
 endmodule
 `default_nettype wire
