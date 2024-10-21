8064e9775239236588ff7431554996a1baf3e06f
Matthew Gehrt <matthewagehrt@gmail.com>
2024-10-20 21:49:12 -0500

    control checkpoint

--- project/demo1/verilog/control.v
+++ project/demo1/verilog/control.v
@@ -9,17 +9,20 @@ module decode ( input   [4:0] opcode,
                 input   [1:0] r_typeALU
                 output  reg [1:0] aluSrc, 
                 output  reg zeroExt, 
-                output  reg memToReg, 
-                output  reg regWrite, 
-                output  reg memRead, 
+                output  reg [1:0]regSrc,
+                output  reg regWrite,
+                output  reg regDest, 
                 output  reg memWrite, 
                 output  reg jump, 
-                output  reg immSource, 
+                output  reg immSrc, 
+                output  reg [2:0]brControl,
                 output  reg [2:0]aluOp, 
                 output  reg invA, 
                 output  reg invB, 
                 output  reg cin, 
-                output  reg [1:0]brControl);
+                output  reg STU
+                output  reg BTR,
+                output  reg setIf);
 
 always @(*) begin 
     casex (opcode)
@@ -28,31 +31,34 @@ always @(*) begin
         5'b010??: begin 
             aluSrc = 2'b10;
             zeroExt = (opcode[2]) ? 1 : 0;
-            memToReg = 0;
+            regSrc = 2'b10;
             regWrite = 1'b1; 
-            memRead = 1'b0; 
+            regDest = 2'b00;
             memWrite = 1'b0; 
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = (~opcode[1]) ? 3'b000 : opcode[2:0]; 
             invA = (~opcode[1] & opcode[0]) ? 1'b1 : 1'b0;
             cin = (~opcode[1] & opcode[0]) ? 1'b1 : 1'b0;
             invB = (opcode[1] & opcode[0]) ? 1'b1 : 1'b0;
+
+            STU = 1'bx;
+            BTR = 1'bx;
+            setIf = 1'b0;
         end
 
         // Shift with immediate
         5'b101??: begin 
             aluSrc = 2'b10; 
             zeroExt = 1'bx;
-            memToReg = 1'b0;
+            regSrc = 2'b10;
             regWrite = 1'b1; 
-            memRead = 1'b0; 
             memWrite = 1'b0;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = opcode[2:0]; 
             invA = 1'b0;
@@ -64,13 +70,12 @@ always @(*) begin
         5'b10000: begin 
             aluSrc = 2'b10; 
             zeroExt = 1'b0;
-            memToReg = 1'b0;
+            regSrc = 2'bxx;
             regWrite = 1'b0; 
-            memRead = 1'b0; 
             memWrite = 1'b1;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = 3'b000;
             invA = 1'b0;
@@ -82,13 +87,12 @@ always @(*) begin
         5'b10001: begin 
             aluSrc = 2'b10; 
             zeroExt = 1'b0;
-            memToReg = 1'b1;
+            regSrc = 2'b01;
             regWrite = 1'b1; 
-            memRead = 1'b1; 
             memWrite = 1'b0;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = 3'b000;
             invA = 1'b0;
@@ -100,13 +104,12 @@ always @(*) begin
         5'b10011: begin 
             aluSrc = 2'b10; 
             zeroExt = 1'b0;
-            memToReg = 1'b0;
+            regSrc = 2'b10;
             regWrite = 1'b1; 
-            memRead = 1'b0; 
             memWrite = 1'b1;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = 3'b000;
             invA = 1'b0;
@@ -118,13 +121,11 @@ always @(*) begin
         5'b11001: begin 
             aluSrc = 2'b;
             zeroExt = 1'b;
-            memToReg = 1'b;
             regWrite = 1'b; 
-            memRead = 1'b; 
             memWrite = 1'b;
             jump = 1'b;
-            immSource = 1'b; 
-            brControl = 2'bxx;
+            immSrc = 1'b; 
+            brControl = 3'b0xx;
 
             aluOp = 3'b;
             invA = 1'b;
@@ -136,13 +137,12 @@ always @(*) begin
         5'b11011: begin 
             aluSrc = 2'b00;
             zeroExt = 1'bx;
-            memToReg = 1'b0;
+            regSrc = 2'b10;
             regWrite = 1'b1; 
-            memRead = 1'b0; 
             memWrite = 1'b0;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = (~r_typeALU[1]) ? 3'b000 : {1'b0, r_typeALU[1:0]};
             invA = (~r_typeALU[1] & r_typeALU[0]) ? 1'b1 : 1'b0;
@@ -154,13 +154,12 @@ always @(*) begin
         5'b11010: begin 
             aluSrc = 2'b00;
             zeroExt = 1'bx;
-            memToReg = 1'b0;
+            regSrc = 2'b10;
             regWrite = 1'b1; 
-            memRead = 1'b0; 
             memWrite = 1'b0;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = {1'b1, r_typeALU[1:0]};
             invA = 1'b0;
@@ -172,31 +171,29 @@ always @(*) begin
         5'b111??: begin 
             aluSrc = 2'b11;
             zeroExt = 1'bx;
-            memToReg = 1'b0;
+            regSrc = 2'b10;
             regWrite = 1'b1; 
-            memRead = 1'b0; 
             memWrite = 1'b0;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = 2'bxx;
+            immSrc = 1'bx; 
+            brControl = 3'b0xx;
 
             aluOp = 3'b000;
             invA = 1'b0;
             cin = 1'b0;
-            invB = 1'b0;
+            invB = (opcode[0] & opcode[1]) ? 1'b0 : 1'b1;
         end
 
         // Branch
         5'b111??: begin 
             aluSrc = 2'b10;
             zeroExt = 1'b1;
-            memToReg = 1'b0;
+            regSrc = 2'bxx;
             regWrite = 1'b0; 
-            memRead = 1'b0; 
             memWrite = 1'b0;
             jump = 1'b0;
-            immSource = 1'bx; 
-            brControl = opcode[1:0];
+            immSrc = 1'bx; 
+            brControl = {1'b1, opcode[1:0]};
 
             aluOp = 3'b000;
             invA = 1'b0;
