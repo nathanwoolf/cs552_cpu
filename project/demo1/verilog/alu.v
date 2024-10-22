24c4b16d040e0e235d7577e2cc20bcfad07d6a2a
nathanwoolf <nathanwoolf38@gmail.com>
2024-10-22 13:56:11 -0500

    alu updated

--- project/demo1/verilog/alu.v
+++ project/demo1/verilog/alu.v
@@ -26,7 +26,7 @@ module alu (InA, InB, Cin, Oper, invA, invB, sign, Out, Zero, Ofl);
     output                      Zero; // Signal if Out is 0
 
     /* YOUR CODE HERE */
-    wire [15:0] A, B, shifter_out, alu_out, A_xor_B, A_or_B, A_and_B; 
+    wire [15:0] A, B, shifter_out, alu_out, A_xor_B, A_and_B; 
     wire alu_Cout; 
 
     assign A = (invA) ? ~InA : InA; 
@@ -36,17 +36,15 @@ module alu (InA, InB, Cin, Oper, invA, invB, sign, Out, Zero, Ofl);
     cla_16b adder(.sum(alu_out), .c_out(alu_Cout), .a(A), .b(B), .c_in(Cin)); 
 
     assign A_and_B = A & B; 
-    assign A_or_B = A | B; 
     assign A_xor_B = A ^ B; 
 
 //TODO: 
     assign Ofl = sign ? ((A[15] & B[15] & ~alu_out[15]) | (~A[15] & ~B[15] & alu_out[15])) : alu_Cout; 
 
-    assign Out =    (~Oper[2]) ? shifter_out : 
-                    (Oper[2] & ~Oper[1] & ~Oper[0]) ? alu_out : 
-                    (Oper[2] & ~Oper[1] & Oper[0]) ? A_and_B : 
-                    (Oper[2] & Oper[1] & ~Oper[0]) ? A_or_B : 
-                    (&Oper) ? A_xor_B : 
+    assign Out =    (Oper[2]) ? shifter_out : 
+                    (~Oper[2] & ~Oper[1]) ? alu_out : 
+                    (~Oper[2] & Oper[1] & ~Oper[0]) ? A_xor_B : 
+                    (~Oper[2] & Oper[1] & Oper[0]) ? A_and_B : 
                     16'h0000;           //default case - should never happen;
 
     assign Zero = ~|Out;
