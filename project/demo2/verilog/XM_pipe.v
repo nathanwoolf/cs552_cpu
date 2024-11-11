dfd5926c3648fd4fbb78c125a3a39b3726366045
nathanwoolf <nathanwoolf38@gmail.com>
2024-11-11 11:55:45 -0600

    oops

--- project/demo2/verilog/XM_pipe.v
+++ project/demo2/verilog/XM_pipe.v
@@ -1,5 +1,5 @@
 module XM_pipe(
-    input clk, input rst,
+    input wire clk, input wire rst,
     input wire [15:0]next_pc,           output wire [15:0]XM_next_pc,      //this is the next pc after jumps and garbage
     input wire [15:0]pc_inc,            output wire [15:0]XM_pc_inc,
     input wire [15:0]aluOut,            output wire [15:0]XM_aluOut, 
@@ -7,11 +7,11 @@ module XM_pipe(
     input wire [15:0]specOps,           output wire [15:0]XM_specOps,       //^ direct outputs from execute 
     input wire DX_memWrite,             output wire XM_memWrite, 
     input wire DX_memRead,              output wire XM_memRead, 
-    input wire [1:0]DX_regSrc,          output wire XM_regSrc   
+    input wire [1:0]DX_regSrc,          output wire [1:0]XM_regSrc   
 ); 
 
 dff NEXT_PC[15:0](.d(next_pc), .q(XM_next_pc), .clk(clk), .rst(rst));
-dff NEXT_PC[15:0](.d(pc_inc), .q(XM_pc_inc), .clk(clk), .rst(rst));
+dff PC_INC[15:0](.d(pc_inc), .q(XM_pc_inc), .clk(clk), .rst(rst));
 dff ALU_OUT[15:0](.d(aluOut), .q(XM_aluOut), .clk(clk), .rst(rst));
 dff OUT_DATA[15:0](.d(outData), .q(XM_writeData), .clk(clk), .rst(rst));
 dff SPEC_OPS[15:0](.d(specOps), .q(XM_specOps), .clk(clk), .rst(rst));
